a9646bc805bf051d5e327c5f74017ed4d4fd174f
nishrit <olinishrit@gmail.com>
2025-08-15 22:08:01 +0545

    adds logo

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,16 @@ import "./_stylesheets/typography.css";
 
 export const metadata = {
 	title: "Golden Hour",
-	description: "One stop to track your Vitamin D intake",
+	description: "Track your sun exposure & vitamin D levels with precision. Real-time tips, weather updates & reminders to keep you healthy every day.",
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/site.webmanifest" />
 			<body>{children}</body>
 		</html>
 	);
