2a8b63dc3c782989d919e68041733103fe24d866
nishrit <olinishrit@gmail.com>
2025-08-16 09:56:37 +0545

    makes something for desktop site

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Ubuntu } from "next/font/google"
+import { Ubuntu } from "next/font/google";
 
 import "./_stylesheets/reset.css";
 import "./_stylesheets/variables.css";
@@ -7,7 +7,8 @@ import "./global.css";
 
 export const metadata = {
 	title: "Golden Hour",
-	description: "Track your sun exposure & vitamin D levels with precision. Real-time tips, weather updates & reminders to keep you healthy every day.",
+	description:
+		"Track your sun exposure & vitamin D levels with precision. Real-time tips, weather updates & reminders to keep you healthy every day.",
 };
 
 const ubuntu = Ubuntu({ weight: ["400", "500", "700"], subsets: ["latin"] });
@@ -15,11 +16,27 @@ const ubuntu = Ubuntu({ weight: ["400", "500", "700"], subsets: ["latin"] });
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
 			<link rel="manifest" href="/site.webmanifest" />
-			<body className={ubuntu.className}>{children}</body>
+			<body className={ubuntu.className}>
+				<main className="app_screen">{children}</main>
+			</body>
 		</html>
 	);
 }
