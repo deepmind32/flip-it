bf4f4d77cf0bedffde693b047bc25d2dc421b6a0
nishrit <olinishrit@gmail.com>
2025-08-16 23:53:02 +0545

    adds weather api:

--- app/_ui/weather/weather.jsx
+++ app/_ui/weather/weather.jsx
@@ -1,22 +1,53 @@
 import styles from "./weather.module.css";
+import SunCalc from "suncalc";
+import { cookies } from "next/headers";
 
-export default function Weather() {
+async function get_weather_details(latitude, longitude) {
+	const get_url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=uv_index_max&current=cloud_cover,temperature_2m`;
+
+	try {
+		const request = await fetch(get_url);
+		const response = await request.json();
+
+		return response;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+function getSolarElevation(lat, lon, date = new Date()) {
+	const pos = SunCalc.getPosition(date, lat, lon);
+	return pos.altitude * (180 / Math.PI); // in degrees
+}
+
+export default async function Weather() {
+	const cookies_store = await cookies();
+	const latitude = cookies_store.get("latitude").value;
+	const longitude = cookies_store.get("longitude").value;
+
+	const weather = await get_weather_details(latitude, longitude);
+	const elevation_angle = getSolarElevation(latitude, longitude);
+
 	return (
 		<div className={styles["progress__wrapper"]}>
 			<div className={styles["progress__goal_daily"]}>
-				<p>Today's Vitamin D goal</p>
-				<h2>60%</h2>
-				<small>3,600 IU out of target of 4,200 IU</small>
+				<p>Solar Elevation Angle</p>
+				<h2>{elevation_angle.toFixed(2)}&deg;</h2>
+				<small>
+					{elevation_angle > 45
+						? "Optimal time for Vitamin D"
+						: "Inefficient time for Vitamin D"}
+				</small>
 			</div>
 
 			<div className={styles["progress__under"]}>
 				<div className={styles["progress__goal_estimated"]}>
 					<p>UV Index</p>
-					<h1>7</h1>
+					<h1>{weather?.["daily"]?.["uv_index_max"][0] ?? "XX"}</h1>
 				</div>
 				<div className={styles["progress__goal_weekly"]}>
-					<p>Weekly Target 32,000 IU</p>
-					<h2>31,000</h2>
+					<p>Cloud Cover</p>
+					<h2>{weather?.["current"]?.["cloud_cover"] ?? "XX"}%</h2>
 				</div>
 			</div>
 		</div>
