89d5292d1a8d0f67ef2881122ab6fdde0a130a55
nishrit <olinishrit@gmail.com>
2025-08-16 09:26:22 +0545

    writes middleware logic to see if userinfo exists

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-	return <div></div>;
+	return (
+		<main className={styles["app"]}>
+			<div className={styles["app__welcome"]}>
+				<p>Good morning, anonymous 123</p>
+				
+			</div>
+		</main>
+	);
 }
---
+++ middleware.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+const REQUIRED_COOKIES = ["name", "age", "gender", "skin_tone"];
+const COOKIES_REQUIRED_PATH = ["/"];
+
+export function middleware(request) {
+	const { pathname } = request.nextUrl;
+
+	const user_info = {
+		name: request.cookies.get("name"),
+		age: request.cookies.get("age"),
+		gender: request.cookies.get("gender"),
+		skin_tone: request.cookies.get("skin_tone"),
+	};
+	console.log(user_info);
+
+	// if any of the cookies is undefined then, redirect them to start page
+	const hasAllCookies = REQUIRED_COOKIES.every((cookie) =>
+		request.cookies.has(cookie)
+	);
+
+	if (COOKIES_REQUIRED_PATH.includes(pathname) && !hasAllCookies) {
+		return NextResponse.redirect(new URL("/start", request.url));
+	}
+
+	// if in the start page but already have all cookies then redict to / page
+	if (hasAllCookies && pathname === "/start") {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/", "/start"],
+};
